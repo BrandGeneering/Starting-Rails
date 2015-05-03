ec183bd8a02fa9a6fc9fdf65cabda51295cb9e44
NeuTrix <NeuTrix@brandgeneering.com>
2015-05-02 21:58:56 -0700

    added strong params and create methods

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -4,8 +4,16 @@ class ArticlesController < ApplicationController
 	end
 
 	def create
-		# @article = Article.new
-		render plain: params[:article].inspect
+		@article = Article.new(article_params))
+		@article.save
+		redirect_to @article
 	end
+	
+# strong params
+private
+	def article_params
+		params.require[:article].permit(:title,:text
+	end
+
 
 end
