d67ddda04708e5263bde4de5cd0f72d19704a96c
NeuTrix <NeuTrix@brandgeneering.com>
2015-05-02 22:25:02 -0700

    set for new article view and finisj 5.6-7

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,18 +1,22 @@
 class ArticlesController < ApplicationController
 
+	def show
+		@article = Article.find(params[:id])
+	end
+
 	def new
 	end
 
 	def create
-		@article = Article.new(article_params))
+		@article = Article.new(article_params)
 		@article.save
 		redirect_to @article
 	end
-	
+
 # strong params
 private
 	def article_params
-		params.require[:article].permit(:title,:text
+		params.require(:article).permit(:title,:text)
 	end
 
 
