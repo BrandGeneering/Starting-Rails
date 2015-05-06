88abc8ab31761793403d83b89890478bc07de091
NeuTrix <NeuTrix@brandgeneering.com>
2015-05-05 21:52:46 -0700

    add destroy form

--- blog/app/controllers/comments_controller.rb
+++ blog/app/controllers/comments_controller.rb
@@ -7,6 +7,10 @@ class CommentsController < ApplicationController
 		redirect_to article_path(@article)
 	end
 
+	def destroy
+		@comment = Comment.where(article_id: params[:article_id])
+	end
+
 	private
 		def comment_params
 			params.require(:comment).permit(:commenter, :body)
